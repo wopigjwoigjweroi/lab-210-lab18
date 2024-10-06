e43e6593a5c7ae2eaa8a897936d116ceba596077
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-05 23:28:12 -0700

    Commit 2

--- Lab18.cpp
+++ Lab18.cpp
@@ -9,6 +9,46 @@ struct Node {
 	string comment; 
 };
 
+  // Function Prototypes
 void headNode(Node *&head, double rating, const string &comment); 
 void tailNode(Node *&head, double rating, const string &comment); 
 void reviews(Node *head); 
+
+void headNode(Node *&head, double rating, const string &comment) {
+
+    Node *newNode = new Node; 
+
+    newNode->value = rating; 
+
+    newNode->comment = comment; 
+
+    newNode->next = head; 
+
+    head = newNode; 
+}
+
+void tailNode(Node *&head, double rating, const string &comment) {
+
+    Node *newNode = new Node;
+
+    newNode->value = rating; 
+
+    newNode->comment = comment; 
+
+    newNode->next = nullptr; 
+
+    if (!head) {
+
+        head = newNode; 
+    } else {
+
+        Node *temp = head; 
+
+        while (temp->next) {
+            temp = temp->next; 
+        }
+            temp->next = newNode; 
+    }
+}
+
+
